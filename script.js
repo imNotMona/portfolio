45d5beeff30e44ca07a64b0c2ffb581cefe9033b
mona <miku.money206@gmail.com>
2026-01-07 04:45:21 -0800

    polish loading
    script to seamless page change + other content yay

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+document.addEventListener("DOMContentLoaded", async () => {
+  const includes = document.querySelectorAll("[data-include]");
+
+  await Promise.all(
+    Array.from(includes).map(async el => {
+      const file = el.getAttribute("data-include");
+      const res = await fetch(file);
+      el.innerHTML = await res.text();
+    })
+  );
+
+  document.body.classList.remove("preload");
+});
